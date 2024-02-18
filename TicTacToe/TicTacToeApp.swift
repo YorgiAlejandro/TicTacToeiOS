cd396f01197bcaff0902c6378d8b8ff038b942ea
Yorgi <yorgidelriomarquez@gmail.com>
2024-02-18 11:23:44 -0500

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by M2 on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
